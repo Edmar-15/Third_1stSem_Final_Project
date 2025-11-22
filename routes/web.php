1437b4ab2f524bed9e89bdecb7bd742cf22be6c4
Rymuel <0323-3653@lspu.edu.ph>
2025-11-22 22:36:04 +0800

    Commit ni Rymuel

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/listahan/delete-user', [UserController::class, 'delete_user'])->nam
 Route::post('/listahan/delete-user/{id}', [UserController::class, 'delete'])->name('user.delete');
 
 Route::get('/listahan/delete-user/{id}', [UserController::class, 'delete_prompt'])->name('user.delete_prompt');
+
+Route::get('/rym', function () {
+    return view('rymuel');
+});
